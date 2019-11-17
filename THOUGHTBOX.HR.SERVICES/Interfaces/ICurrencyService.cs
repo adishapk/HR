d1092949dadc78e67b8f886588700f3d0c5e9744
adisha <adishapktirur@gmail.com>
2019-11-17 09:39:04 +0300

    Revert "Deleted service"
    This reverts commit 88e0a831c6ae757113c545094fef40afaec5cdb4.

---
+++ THOUGHTBOX.HR.SERVICES/Interfaces/ICurrencyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using THOUGHTBOX.DOMAIN.Domain;
+using System.Data;
+
+namespace THOUGHTBOX.HR.SERVICES.Interfaces
+{
+    public interface ICurrencyService
+    {
+        int Create(Currency currency);
+        int Delete(Int64 Id);
+        int Update(Currency currency);
+        String GetAll();
+        Currency GetById(Int64 Id);
+    }
+}
